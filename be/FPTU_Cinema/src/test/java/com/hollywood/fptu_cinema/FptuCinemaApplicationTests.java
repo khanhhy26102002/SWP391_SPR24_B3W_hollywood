587c066af496d14b454996d5033c6263717adada
truonghoang <boybybame@gmail.com>
2024-04-15 11:56:00 +0700

    add project be

---
+++ be/FPTU_Cinema/src/test/java/com/hollywood/fptu_cinema/FptuCinemaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hollywood.fptu_cinema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FptuCinemaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
