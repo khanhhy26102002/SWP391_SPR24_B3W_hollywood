5daf72dd85b77030d3998070f4287773780a3dea
truonghoang <boybybame@gmail.com>
2024-04-19 01:40:23 +0700

    fix code

--- be/FPTU_Cinema/src/main/java/com/hollywood/fptu_cinema/model/User.java
+++ be/FPTU_Cinema/src/main/java/com/hollywood/fptu_cinema/model/User.java
@@ -14,6 +14,7 @@ import java.time.LocalDate;
 @Table(name = "User", schema = "Movie_Booking_Ticket")
 public class User {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_id", nullable = false)
     private Integer id;
 
