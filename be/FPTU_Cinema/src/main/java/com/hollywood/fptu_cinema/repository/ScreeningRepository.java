2100027cda16bc5d5fe93af1166ad1614d334231
truonghoang <boybybame@gmail.com>
2024-04-22 03:26:13 +0700

    fix Create Booking and Payment

--- be/FPTU_Cinema/src/main/java/com/hollywood/fptu_cinema/repository/ScreeningRepository.java
+++ be/FPTU_Cinema/src/main/java/com/hollywood/fptu_cinema/repository/ScreeningRepository.java
@@ -2,10 +2,17 @@ package com.hollywood.fptu_cinema.repository;
 
 import com.hollywood.fptu_cinema.model.Screening;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.Instant;
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 public interface ScreeningRepository extends JpaRepository<Screening, Integer> {
-    List<Screening> findFinishedScreenings(Instant now);
+    @Query("SELECT s FROM Screening s WHERE s.endTime < :instant")
+    List<Screening> findFinishedScreenings(@Param("instant") Instant instant);
+
+    Optional<Screening> findByDateAndStartTime(LocalDate date, Instant startTime);
 }
