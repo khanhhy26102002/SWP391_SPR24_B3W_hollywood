711b75515ba05af99929cc14c0941347a2fc4e31
KhacTri16 <trihoang12345z@gmail.com>
2024-04-21 22:18:41 +0700

    sua lai screening repository

--- be/FPTU_Cinema/src/main/java/com/hollywood/fptu_cinema/repository/ScreeningRepository.java
+++ be/FPTU_Cinema/src/main/java/com/hollywood/fptu_cinema/repository/ScreeningRepository.java
@@ -2,10 +2,13 @@ package com.hollywood.fptu_cinema.repository;
 
 import com.hollywood.fptu_cinema.model.Screening;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.Instant;
 import java.util.List;
 
 public interface ScreeningRepository extends JpaRepository<Screening, Integer> {
-    List<Screening> findFinishedScreenings(Instant now);
+    @Query("SELECT s FROM Screening s WHERE s.endTime < :instant")
+    List<Screening> findFinishedScreenings(@Param("instant") Instant instant);
 }
