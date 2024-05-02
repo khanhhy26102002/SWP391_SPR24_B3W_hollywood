e264208fb28e46900eaade21c925b197f54db412
truonghoang <boybybame@gmail.com>
2024-05-02 23:06:30 +0700

    fix code

--- be/FPTU_Cinema/src/main/java/com/hollywood/fptu_cinema/repository/TicketRepository.java
+++ be/FPTU_Cinema/src/main/java/com/hollywood/fptu_cinema/repository/TicketRepository.java
@@ -4,15 +4,11 @@ import com.hollywood.fptu_cinema.model.Ticket;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-import java.math.BigDecimal;
 import java.time.Instant;
 import java.util.List;
 
 public interface TicketRepository extends JpaRepository<Ticket, Integer> {
     List<Ticket> findTicketsByExpirationTimeBefore(Instant now);
-
-    @Query("SELECT SUM(t.totalPrice) FROM Ticket t WHERE t.status = 2 AND t.createdDate BETWEEN :start AND :end")
-    BigDecimal calculateRevenueInDateRange(Instant start, Instant end);
 
     @Query("SELECT COUNT(t) FROM Ticket t WHERE t.status = 2 AND t.createdDate BETWEEN :start AND :end")
     long countByCreatedDateBetweenAndStatusIsSold(Instant start, Instant end);
